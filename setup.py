2c77dbdef45d6c076b619a567649132df3250d2c
AeroNotix <aaron.l.france@gmail.com>
2011-12-07 15:57:16 +0100

    Cleaning house

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+"""
+py2exe conversion script
+"""
+
 from distutils.core import setup
-import py2exe
+#import py2exe
 
 setup(windows=[{"script":"main.pyw"}], options={"py2exe":{"includes":["sip"]}})
