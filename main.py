9a329a8487aa1473e82262ea5e280702fd8a40e2
AeroNotix <aaron.l.france@gmail.com>
2011-12-07 15:41:53 +0100

    PyLint changes

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ Module for converting PDF files to tiff files en masse
 import os
 import sys
 import subprocess
-import time
+#import time
 import threading
 from Queue import Queue
 
 from PyQt4 import QtGui
-import Image, ImageFilter, ImageEnhance
+#import Image, ImageFilter, ImageEnhance
 
 from pyPdf import PdfFileWriter, PdfFileReader
 from scanning_tools.main_UI import Ui_MainWindow
@@ -31,10 +31,11 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         """Init Window"""
         QtGui.QMainWindow.__init__(self, parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.gui = Ui_MainWindow()
+        self.gui.setupUi(self)
         self.gscriptpath = '"' +  os.getcwd() + r'\gs\gs9.02\bin'
-        self.ui.progressBar.hide()
+        self.gui.progressBar.hide()
+        self.single_output_dir = ''
 
     def dir_locate(self):
         """
@@ -54,13 +55,13 @@ class MainWindow(QtGui.QMainWindow):
         Spawns a find file dialog
         """
 
-        self.dir_dialog = QtGui.QFileDialog(self)
-        self.dir_dialog.setFileMode(QtGui.QFileDialog.Directory)
+        dir_dialog = QtGui.QFileDialog(self)
+        dir_dialog.setFileMode(QtGui.QFileDialog.Directory)
 
-        if self.dir_dialog.exec_() == True:
-            for item in self.dir_dialog.selectedFiles():
+        if dir_dialog.exec_() == True:
+            for item in dir_dialog.selectedFiles():
                 self.single_output_dir = item
-                self.ui.single_line_out.setText(item)
+                self.gui.single_line_out.setText(item)
                 break
 
 
@@ -71,18 +72,22 @@ class MainWindow(QtGui.QMainWindow):
         creates a dialog to find a single file
         """
 
-        self.ui.single_line_in.setText(QtGui.QFileDialog.getOpenFileName(
+        self.gui.single_line_in.setText(QtGui.QFileDialog.getOpenFileName(
                                        self, 'Open', '' ,('PDF Files (*.pdf)')))
 
 
     def convert(self):
 
         """
-        Attempted implementation of multithreaded processing
+        Implementation of multithreaded processing
         """
-        def producer(q, pdf):
+        def producer(queue, pdf):
 
-            self.deletions = []
+            """
+            Produces jobs for the consumer to monitor
+            """
+
+            deletions = []
             # Loop through the PDF creating a output stream
             # and putting the single page into it.
             for i in xrange(pdf.numPages):
@@ -92,7 +97,7 @@ class MainWindow(QtGui.QMainWindow):
 
 
                 # append file name of the pdf to the list, so we can tidy up
-                self.deletions.append(
+                deletions.append(
                             str(self.single_output_dir +
                             "\page%s.pdf" % i).replace('/', '\\'))
 
@@ -123,30 +128,47 @@ class MainWindow(QtGui.QMainWindow):
                             '"').replace('/', '\\'))      # WTF IS THIS SHIT
 
                 thread.start()
-                q.put(thread, True)
+                queue.put(thread, True)
 
 
-        def consumer(q, total_files):
-            finished = 0
+        def consumer(queue, total_files):
+
+            """
+            Receives jobs and blocks whilst they complete
+            """
+
+            finished = 0 # task counter
+
             while finished < total_files:
-                thread = q.get(True)
+
+                # get tasks
+                thread = queue.get(True)
+
+                # block whilst running
                 thread.join()
-                finished+= 1
+                finished += 1
 
-        q = Queue(30)
-        pdffname = PdfFileReader(open(self.ui.single_line_in.text(), 'rb'))
 
+        # Queue size, we can play with this.
+        queue = Queue(30)
+
+        # open pdf and assign local var
+        pdffname = PdfFileReader(open(self.gui.single_line_in.text(), 'rb'))
+
+        # init threads
         prod_thread = threading.Thread(target=producer,
-                                       args=(q, pdffname))
-        cons_thread = threading.Thread(target=consumer,
-                                       args=(q, pdffname.numPages))
+                                       args=(queue, pdffname))
 
-        prod_thread.start()
+
+        cons_thread = threading.Thread(target=consumer,
+                                       args=(queue, pdffname.numPages))
+
+        prod_thread.start()    # Start them engines!
         cons_thread.start()
-        prod_thread.join()
+        prod_thread.join()     # Ensure block!
         cons_thread.join()
 
-        #cleanup(self.deletions)
+        #cleanup(deletions)
 
 
 
@@ -161,6 +183,9 @@ def cleanup(deletions):
         os.remove(fname)
 
 def encase(string, target):
+    """
+    Encases a string in another string
+    """
     return string+target+string
 
 #-------------------------------Multithreading----------------------------------
@@ -186,7 +211,7 @@ class PDFConverter(threading.Thread):
         Uses ghostscript from the command line
         """
 
-        cmd = subprocess.Popen(' '.join([
+        subprocess.Popen(' '.join([
                            self.gscriptpath + '\gswin32c.exe"',   #gs exe
                            '-q',
                            '-dNOPAUSE',
@@ -194,21 +219,13 @@ class PDFConverter(threading.Thread):
                            '-r900',       # resolution
                            '-sDEVICE=tiffg4',  # container type, see gs docs
                            '-sPAPERSIZE=a4',   # page size
-                           '-sOutputFile=%s %s' % (str(self.ofname), str(self.ifname)),
+                           '-sOutputFile=%s %s' % (str(self.ofname),
+                                                   str(self.ifname))
+
                            ]), shell=True)  # don't spawn cmd window
 
         #cmd.wait()
 
-class ProgressUpdater(threading.Thread):
-    """
-    Offloads updating the progress bar to a thread
-    """
-
-    def __init__(self, bar):
-        self.bar = bar
-
-    def run(self):
-        bar.setValue(bar.value() + 1)
 
 
 if __name__ == "__main__":
