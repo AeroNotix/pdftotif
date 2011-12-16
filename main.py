5b36c9040d7eb3800225775d6cf7e0feedcb3fac
AeroNotix <aaron.l.france@gmail.com>
2011-12-16 11:04:17 +0100

    QThreadPool is the final version of threading

--- main.py
+++ main.py
@@ -13,21 +13,14 @@ be beneficial elsewhere.
 #---------------------------Imports---------------------------------------------
 import os
 import sys
-import time
 
 from PyQt4 import QtGui
-from PyQt4.QtCore import SIGNAL, QThreadPool, QReadWriteLock
+from PyQt4.QtCore import QThreadPool
 
 from pyPdf import PdfFileReader
 
 from scanning_qthread.ui.main_UI import Ui_MainWindow
-from scanning_qthread.mthreading.mthreading import (QRunner,
-                                                   QWorker,
-                                                   QFileCleaner,
-                                                   QFileWorker,
-                                                   QThreadHandle)
-
-from scanning_qthread.misc.misc import encase, cleanup
+from scanning_qthread.mthreading.mthreading import QFileWorker, QThreadHandle
 
 #---------------------------GUI Class-------------------------------------------
 
@@ -50,10 +43,16 @@ class MainWindow(QtGui.QMainWindow):
         self.single_output_dir = ''
         self.threads = []
         self.deletions = []
-        self.lock = QReadWriteLock()
+        self.work = QThreadPool()
+        self.work.setMaxThreadCount(1)
 
     def quit(self):
-        print 'quitted'
+
+        """
+        Quit the window, in case we need some specific behaviour
+        """
+
+        print self
 
     def dir_locate(self):
         """
@@ -90,6 +89,8 @@ class MainWindow(QtGui.QMainWindow):
         creates a dialog to find a single file
         """
 
+        # Create the file lookup dialog using built-in dialogs
+        # We set the file type to PDF and only PDF
         self.gui.single_line_in.setText(QtGui.QFileDialog.getOpenFileName(
                                        self, 'Open', '' ,('PDF Files (*.pdf)')))
 
@@ -100,24 +101,30 @@ class MainWindow(QtGui.QMainWindow):
         Method to update progress bar whilst running conversion
         """
 
-        self.lock.lockForRead()
-        curr_progress = self.gui.progressBar.value() + 1
-        print self.gui.progressBar.value()
-        self.lock.unlock()
+        # When we get a call it's from a thread finishing
+        # we increment the progress bar and check if we're at 100%
+        self.gui.progressBar.setValue(self.gui.progressBar.value()+1)
 
-        self.lock.lockForWrite()
-        self.gui.progressBar.setValue(curr_progress)
-        self.lock.unlock()
-
-        APPLICATION.processEvents()
-
-
-
+        # This is bad, if a QProcess fails, we don't get a tick
+        # and the progressBar will never get to 100%
+        # we need to implement something that will catch errors with
+        # QProcess
         if self.gui.progressBar.value() == self.gui.progressBar.maximum():
             self.gui.progressBar.hide()
+
+            # create the deleter thread with a reference to the deletions
+            # list.
             deleter = QFileWorker(self.deletions, self)
+
+            # wait for the deletions thread to return
             deleter.threadpool.waitForDone()
+
+            # re-init deletions list so we don't try to delete
+            # already deleted files next time
             self.deletions = []
+
+            # Re-enable the button
+            self.gui.btn_single_convert.setEnabled(True)
 
 
     def convert(self):
@@ -125,31 +132,44 @@ class MainWindow(QtGui.QMainWindow):
         """
         Implementation of multithreaded processing
         """
-        self.gui.btn_dir_convert.setEnabled(False) # disable button
+        self.gui.btn_single_convert.setEnabled(False) # disable button
         self.gui.progressBar.setValue(0) # re-init progress bar
+
+
+        # Open the PDF that we found in the dialog
         pdf = PdfFileReader(open(self.gui.single_line_in.text(), 'rb'))
 
+        # Show the progress bar
         self.gui.progressBar.show()
+
+        # Set the progress bar's maximum number to the number of pages
+        # in the PDF
         self.gui.progressBar.setMaximum(pdf.numPages)
 
-        APPLICATION.processEvents()
+        # Send the PDF object (PdfFileReader) to the ThreadHandler
+        # along with a reference to ourself in order to set up signal
+        # callbacks
 
-        start = time.clock() # debug
+        # most of the magic is in mthreading.py
+        # this is the cleanest interface I could come up with
+        # just pass it an opened PDF document and processing will begin
+        self.work.start(QThreadHandle(pdf, self))
 
-        work = QThreadPool()
-        work.setMaxThreadCount(1)
-        work.start(QThreadHandle(pdf, self))
-
-        print time.clock() - start # debug
 
     def convert_dir(self):
+        """
+        Method to convert a whole directory
+        """
         pass
 
 
 
 
 if __name__ == "__main__":
+
+
     APPLICATION = QtGui.QApplication(sys.argv)
     MAINWINDOW = MainWindow()
     MAINWINDOW.show()
     sys.exit(APPLICATION.exec_())
+
--- mthreading/mthreading.py
+++ mthreading/mthreading.py
@@ -29,7 +29,6 @@ class QFileCleaner(QRunnable):
         QRunnable.__init__(self)
         self.deletions = deletions
         self.cls = cls
-        self.q_object = QObject()
 
     def run(self):
 
@@ -39,17 +38,19 @@ class QFileCleaner(QRunnable):
 
         for fname in self.deletions:
             os.remove(fname)
-        try:
-            self.cls.gui.pushButton.setEnabled(True)
-        except AttributeError:
-            pass
 
 
-class QFileWorker(QObject):
+
+class QFileWorker(object):
 
     def __init__(self, deletions, cls):
-        """init attribs"""
-        QObject.__init__(self)
+        """
+        This class handles deletion of temporary file objects once processing
+        has completed.
+
+        It takes a reference to the class because we re-enable it's button
+        because we disabled it during processing
+        """
 
         self.deletions = deletions
         self.threadpool = QThreadPool()
@@ -73,14 +74,29 @@ class QRunner(QRunnable):
     """
 
     def __init__(self, ifname, ofname):
+
         """
         This class does the work of moving the files to the executable to
         process the pdf pages.
+
+        This class should not be called directly as it is a QRunnable object
+        therefore we need a QThreadPool object to start/stop and monitor
+        progress
+
+        The worker is QWorker and will create these instances for us.
         """
+
+        # Init parent object
         QRunnable.__init__(self)
+
+        # create an instance of a QObject so we can emit signals
         self.q_object = QObject()
+
+        # take the inputs
         self.ofname = ofname
         self.ifname = ifname
+
+        # gs exe
         self.gscriptpath = '"' +  os.getcwd() + r'\gs\gs9.02\bin'
 
     def run(self):
@@ -127,25 +143,83 @@ class QWorker(QObject):
     """
 
     def __init__(self):
-        """init attribs"""
+
+        """
+        This class creates new instances of QRunner, which is the class
+        that directly interfaces with the external executable
+
+        We need to create the QRunner/QWorker routine because a QRunnable
+        does not inherit from QObject and therefore does not have access
+        to the signal/slot mechanisms that we need in order to receive when
+        things get done.
+
+        QWorker inherits from QObject, so we need to directly call QObjects
+        constructor to initialize the underlying C++ object, super() does not
+        allow for this (seemingly) so we need to call it directly.
+        """
+
+        # init QObject
         QObject.__init__(self)
+
+        # Our own threadpool, for adding QRunners
         self.threadpool = QThreadPool()
 
     def create_job(self, ifname, ofname, cls):
+
+        """
+        This method takes three arguments:
+
+        ifname is the input file name of the file to be converted
+        ofname is a string which is to be the output of the filename
+        cls is a reference to the calling class, so that we can connect
+        a signal to a slot.
+        """
+
+        # Create the QRunner object and pass it filenames
         runner = QRunner(ifname, ofname)
+
+        # using our own connect method inherited from QObject
+        # connect the QRunner created before and use it's QObject
+        # to connect a signal to the slot
         self.connect(runner.q_object, SIGNAL(
                                          "finished()"), cls.update_progress_bar)
-        self.threadpool.start(runner)
-        #self.threadpool.waitForDone()
 
+        # ask our threadpool to run the task
+        self.threadpool.start(runner)
 
 
 class QThreadHandle(QRunnable):
 
     def __init__(self, pdf, cls):
+
+        """
+        This is the only class that gets called from the main thread
+
+        It is called when we wish to split a PDF document and when we
+        want to start a new batch of threads for image processing
+
+        This class is a thread of it's own, controlling other threads
+
+        I believe this is a good method as it behaves as below:
+
+        Main thread
+            |
+        Call to QThreadHandle----> Start image processing
+            |                               |
+        Main thread carries on              |
+                                         output
+
+        Requires a PdfFileReader object and a reference to the calling class
+        """
+
+        # init parent
         QRunnable.__init__(self)
+
+        # take refs
         self.pdf = pdf
         self.cls = cls
+
+        # create QWorker object
         self.work = QWorker()
         self.work.threadpool.setMaxThreadCount(5)
 
@@ -188,8 +262,5 @@ class QThreadHandle(QRunnable):
                         self.cls.single_output_dir +
                         "\\page%s.tif" % i +
                         '"').replace('/', '\\'), self.cls)   # WTF IS THIS SHIT
-
-            #self.work.threadpool.waitForDone()
-
 
 
